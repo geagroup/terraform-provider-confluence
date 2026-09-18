b46c4c2f08c28ca90a094519715ee661a4a2d454
Cuber, Sebastien <sebastien.cuber@gea.com>
2026-09-18 08:38:03 +0200

    Make space parameter optional in resourceContent schema

--- confluence/resource_content.go
+++ confluence/resource_content.go
@@ -25,7 +25,7 @@ func resourceContent() *schema.Resource {
 			},
 			"space": {
 				Type:        schema.TypeString,
-				Required:    true,
+				Required:    false,
 				ForceNew:    true,
 				DefaultFunc: schema.EnvDefaultFunc("CONFLUENCE_SPACE", nil),
 			},
