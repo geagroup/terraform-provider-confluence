7d57a6cabb641d80b50f6189870b40a6c8cfecd8
Cuber, Sebastien <sebastien.cuber@gea.com>
2026-09-18 06:34:13 +0200

    Update GitHub Actions workflow to use Go 1.24 and improve caching steps

--- examples/content/main.tf
+++ examples/content/main.tf
@@ -18,7 +18,7 @@ resource "confluence_content" "example" {
   body = templatefile("${path.module}/example.tmpl", {
     pets = [for p in random_pet.pets : title(p.id)]
   })
-  labels = ["pets", "examples"]
+  labels = ["pets", "example"]
 }
 
 terraform {
